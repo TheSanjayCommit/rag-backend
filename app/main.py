17b6ac17bd7b99e96cdccf852e716be0d3fa49c2
sanjaynalamasa <sanjaynalamasa07@gmail.com>
2026-04-19 14:30:52 +0530

    fix: allow all CORS origins for production

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "*").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
